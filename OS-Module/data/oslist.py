d1c441838e9a8960a7e94ee9acfc00ae3aad6e78
Shahzaibjee786 <pc19574.shahzaib@gmail.com>
2024-01-22 16:48:27 +0500

    basic of python with problems

---
+++ OS-Module/data/oslist.py
@@ -0,0 +1,6 @@
+import os
+# folder = os.listdir("data")
+# print(folder)
+
+# folder = os.listdir("data/tuts1")
+# print(folder)
---
+++ OS-Module/main.py
@@ -0,0 +1,12 @@
+import os
+# if(not os.path.exists("data")):
+#     os.mkdir("data")
+# for i in range(1,101):
+#     os.mkdir(f"data/day {i}")
+
+# for i in range(1,21):
+#     os.mkdir(f"data/day 1/tutorial {i}")
+
+# for i in range(1,101):
+#     os.rename(f"data/day {i}", f"data/tuts{i}")
+
